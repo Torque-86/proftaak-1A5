e3ade65fe7b99adcf5753ad3a6cb120ddcb72d0a
Inge <ikerk79@hotmail.com>
2019-12-22 16:52:49 +0100

    Deleted stored procedures

--- DML.sql
+++ DML.sql
@@ -5,30 +5,62 @@ Content: Data Manipulation Language (DML)
 
 Query-overzicht:
 1. Wat zijn de parkeerkosten voor Reserveringsnummer 2
+1a. Controle-overzicht van de berekening parkeerkosten
 2. Haal factuur op voor factuurnummer 2 inclusief berekening parkeerkosten
 3. Haal alle facturen op met naam en adres
-4. Haal alle facturen met factuurregels op met naam en adres
-5. Som de parkeeromgevingen op van veel naar weinig gebruikt
-6. Haal de klant op die het meest heeft geparkeerd
-7. Welk bedrijf heeft de meeste medewerkers die van PPP gebruik maken
+4. Som de parkeeromgevingen op van veel naar weinig gebruikt
+5. Haal de klant op die het meest heeft geparkeerd
+6. Welk bedrijf heeft de meeste medewerkers die van PPP gebruik maken
 
-Stored Procedures:
-A. GetAllCustomers()
 ***********************************************************
 */
 
 /***
-* Vergeten de Begin- en EindBetaaldParkerenDag mee te nemen  * in de berekening. Probeer ik nog.
-***/
+* Nog niet uitgevogeld hoe je Begin- en EindBetaaldParkerenDag mee moet nemen in de berekening.
+*/
 
 /* 1. Wat zijn de parkeerkosten voor Reserveringsnummer 2 */
-SELECT TariefUur / 3600 * (SELECT TIME_TO_SEC(TIMEDIFF      
-                           (EindTijd, BeginTijd)) 
-                           FROM PARKEERSESSIE 
-                           WHERE ReserveringsNr = 2) 
-                           AS ParkeerKosten
-FROM PARKEEROMGEVING
+SELECT 
+    ReserveringsNr,
+    CONCAT_WS(' ', PO.Straat, PO.HuisNr, PO.Stad) AS ParkeerOmgeving,
+    BeginTijd,
+    EindTijd,
+    TariefUur / 3600 * (SELECT TIME_TO_SEC(TIMEDIFF      
+                        (EindTijd, BeginTijd)) 
+                        FROM PARKEERSESSIE 
+                        WHERE ReserveringsNr = 2) 
+                        AS ParkeerKosten
+FROM PARKEERSESSIE PS
+LEFT JOIN PARKEEROMGEVING PO ON PS.ParkeerOmgeving = PO.ParkeerOmgevingId
+WHERE ReserveringsNr = 2;
+
+/* 1a Controle van de berekening parkeerkosten */
+SELECT 
+	ReserveringsNr,
+    ParkeerOmgeving,
+	TIMEDIFF(EindTijd, BeginTijd) AS Duur,
+    TIME_TO_SEC(TIMEDIFF(EindTijd, BeginTijd)) AS Seconden
+FROM PARKEERSESSIE 
+WHERE ReserveringsNr = 2;
+                        
+SELECT 
+    ParkeerOmgevingId,
+	TariefUur AS Uurtarief,
+    TariefUur / 3600 AS Rekentarief
+FROM ParkeerOmgeving
 WHERE ParkeerOmgevingId = 2;
+
+SELECT 
+    ReserveringsNr,
+    ParkeerOmgevingId,
+    TariefUur / 3600 * (SELECT TIME_TO_SEC(TIMEDIFF      
+                        (EindTijd, BeginTijd)) 
+                        FROM PARKEERSESSIE 
+                        WHERE ReserveringsNr = 2) 
+                        AS ParkeerKosten
+FROM PARKEERSESSIE PS
+LEFT JOIN PARKEEROMGEVING PO ON PS.ParkeerOmgeving = PO.ParkeerOmgevingId
+WHERE ReserveringsNr = 2;
 
 /* 2. Haal factuur op voor factuurnummer 2 inclusief berekening parkeerkosten */
 SELECT
@@ -56,33 +88,19 @@ SELECT
     F.FactuurNr,
     F.FactuurDatum,
     FR.Kenteken,
-    CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS Naam,
-    CONCAT_WS(' ', Straat, HuisNr, Stad) AS Adres,
-    Parkeersessie,
-    ParkeerOmgeving
-FROM FACTUUR F
-LEFT JOIN FACTUURREGELS FR ON F.FactuurNr = FR.FactuurNr
-LEFT JOIN ADRES A ON F.KlantAdres = A.AdresId
-LEFT JOIN KLANT K ON F.KlantId = K.KlantId;
-
-/* 4. Haal alle facturen met factuurregels op met naam en adres */
-SELECT
-    F.FactuurNr,
-    F.FactuurDatum,
-    FR.Kenteken,
     CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS KlantNaam,
     CONCAT_WS(' ', A.Straat, A.HuisNr, A.Stad) AS KlantAdres,
-    PS.BeginTijd,
-    PS.EindTijd,
-    CONCAT_WS(' ', PO.Straat, PO.HuisNr, PO.Stad) AS ParkeerOmgeving
+    CONCAT_WS(' ', PO.Straat, PO.HuisNr, PO.Stad) AS ParkeerOmgeving,
+    BeginTijd,
+    EindTijd
 FROM FACTUUR F
 LEFT JOIN FACTUURREGELS FR ON F.FactuurNr = FR.FactuurNr
 LEFT JOIN ADRES A ON F.KlantAdres = A.AdresId
 LEFT JOIN KLANT K ON F.KlantId = K.KlantId
-LEFT JOIN PARKEERSESSIE PS ON FR.Kenteken = PS.Kenteken
-LEFT JOIN PARKEEROMGEVING PO ON PS.ParkeerOmgeving = PO.ParkeerOmgevingId;
+LEFT JOIN PARKEEROMGEVING PO ON FR.ParkeerOmgeving = PO.ParkeerOmgevingId
+LEFT JOIN PARKEERSESSIE PS ON FR.ParkeerSessie = PS.ReserveringsNr;
 
-/* 5. Som de parkeeromgevingen op van veel naar weinig gebruikt */
+/* 4. Som de parkeeromgevingen op van veel naar weinig gebruikt */
 SELECT 
     ParkeerOmgevingId AS Id,
 	CONCAT_WS(' ', Straat, HuisNr, Stad) AS ParkeerOmgeving,
@@ -92,7 +110,7 @@ LEFT JOIN PARKEERSESSIE PS ON PO.ParkeerOmgevingId = PS.ParkeerOmgeving
 GROUP BY ParkeerOmgeving
 ORDER BY AantalKeer DESC;
 
-/* 6. Haal de klant op die het meest heeft geparkeerd */
+/* 5. Haal de klant op die het meest heeft geparkeerd */
 SELECT 
     PS.Kenteken,
     CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS KlantNaam,
@@ -105,8 +123,4 @@ ORDER BY AantalKeer DESC
 LIMIT 1;
 
 
-/* 7. Welk bedrijf heeft de meeste medewerkers die van PPP gebruik maken */
-
-
-
-/* A. GetAllCustomers() */
+/* 6. Welk bedrijf heeft de meeste medewerkers die van PPP gebruik maken */
