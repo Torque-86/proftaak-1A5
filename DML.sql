2435808226192f4b3afd012b43a2c80f84b3f374
Inge <ikerk79@hotmail.com>
2019-12-23 12:13:26 +0100

    Naamaanpassingen DDL en RM

--- DML.sql
+++ DML.sql
@@ -22,44 +22,44 @@ Query-overzicht:
 /* 1. Wat zijn de parkeerkosten voor Reserveringsnummer 2 */
 SELECT 
     ReserveringsNr,
-    CONCAT_WS(' ', PO.Straat, PO.HuisNr, PO.Stad) AS ParkeerOmgeving,
+    CONCAT_WS(' ', PP.Straat, PP.HuisNr, PP.Stad) AS ParkeerPlaats,
     BeginTijd,
     EindTijd,
     TariefUur / 3600 * (SELECT TIME_TO_SEC(TIMEDIFF      
                         (EindTijd, BeginTijd)) 
-                        FROM PARKEERSESSIE 
+                        FROM ParkeerSessie 
                         WHERE ReserveringsNr = 2) 
                         AS ParkeerKosten
-FROM PARKEERSESSIE PS
-LEFT JOIN PARKEEROMGEVING PO ON PS.ParkeerOmgeving = PO.ParkeerOmgevingId
+FROM ParkeerSessie PS
+LEFT JOIN ParkeerPlaats PP ON PS.ParkeerPlaats = PP.ParkeerPlaatsId
 WHERE ReserveringsNr = 2;
 
 /* 1a Controle van de berekening parkeerkosten */
 SELECT 
 	ReserveringsNr,
-    ParkeerOmgeving,
+    ParkeerPlaats,
 	TIMEDIFF(EindTijd, BeginTijd) AS Duur,
     TIME_TO_SEC(TIMEDIFF(EindTijd, BeginTijd)) AS Seconden
-FROM PARKEERSESSIE 
+FROM ParkeerSessie 
 WHERE ReserveringsNr = 2;
                         
 SELECT 
-    ParkeerOmgevingId,
+    ParkeerPlaatsId,
 	TariefUur AS Uurtarief,
     TariefUur / 3600 AS Rekentarief
-FROM ParkeerOmgeving
-WHERE ParkeerOmgevingId = 2;
+FROM ParkeerPlaats
+WHERE ParkeerPlaatsId = 2;
 
 SELECT 
     ReserveringsNr,
-    ParkeerOmgevingId,
+    ParkeerPlaatsId,
     TariefUur / 3600 * (SELECT TIME_TO_SEC(TIMEDIFF      
                         (EindTijd, BeginTijd)) 
                         FROM PARKEERSESSIE 
                         WHERE ReserveringsNr = 2) 
                         AS ParkeerKosten
-FROM PARKEERSESSIE PS
-LEFT JOIN PARKEEROMGEVING PO ON PS.ParkeerOmgeving = PO.ParkeerOmgevingId
+FROM ParkeerSessie PS
+LEFT JOIN ParkeerPlaats PP ON PS.ParkeerPlaats = PP.ParkeerPlaatsId
 WHERE ReserveringsNr = 2;
 
 /* 2. Haal factuur op voor factuurnummer 2 inclusief berekening parkeerkosten */
@@ -69,18 +69,19 @@ SELECT
     FR.Kenteken,
     CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS KlantNaam,
     CONCAT_WS(' ', A.Straat, A.HuisNr, A.Stad) AS KlantAdres,
-    Parkeersessie,
-    ParkeerOmgeving,
-    (SELECT TariefUur / 3600 * (SELECT TIMEDIFF(EindTijd, BeginTijd) 
-                                FROM PARKEERSESSIE 
+    CONCAT_WS(' ', PP.Straat, PP.HuisNr, PP.Stad) AS ParkeerPlaats,
+    (SELECT TariefUur / 3600 * (SELECT TIMEDIFF(EindTijd,   
+                                BeginTijd) 
+                                FROM ParkeerSessie 
                                 WHERE ReserveringsNr = 2) 
-     FROM PARKEEROMGEVING 
-     WHERE ParkeerOmgevingId = 2) 
+     FROM ParkeerPlaats 
+     WHERE ParkeerPlaatsId = 2) 
      AS ParkeerKosten
-FROM FACTUUR F
-LEFT JOIN FACTUURREGELS FR ON F.FactuurNr = FR.FactuurNr
-LEFT JOIN ADRES A ON F.KlantAdres = A.AdresId
-LEFT JOIN KLANT K ON F.KlantId = K.KlantId
+FROM Factuur F
+LEFT JOIN FactuurRegel FR ON F.FactuurNr = FR.FactuurNr
+LEFT JOIN Adres A ON F.AdresId = A.AdresId
+LEFT JOIN Klant K ON F.KlantId = K.KlantId
+LEFT JOIN ParkeerPlaats PP ON FR.ParkeerPlaats =  PP.ParkeerPlaatsId
 WHERE F.FactuurNr = 2;
 
 /* 3. Haal alle facturen op met naam en adres */
@@ -90,24 +91,24 @@ SELECT
     FR.Kenteken,
     CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS KlantNaam,
     CONCAT_WS(' ', A.Straat, A.HuisNr, A.Stad) AS KlantAdres,
-    CONCAT_WS(' ', PO.Straat, PO.HuisNr, PO.Stad) AS ParkeerOmgeving,
+    CONCAT_WS(' ', PP.Straat, PP.HuisNr, PP.Stad) AS ParkeerPlaats,
     BeginTijd,
     EindTijd
-FROM FACTUUR F
-LEFT JOIN FACTUURREGELS FR ON F.FactuurNr = FR.FactuurNr
-LEFT JOIN ADRES A ON F.KlantAdres = A.AdresId
-LEFT JOIN KLANT K ON F.KlantId = K.KlantId
-LEFT JOIN PARKEEROMGEVING PO ON FR.ParkeerOmgeving = PO.ParkeerOmgevingId
-LEFT JOIN PARKEERSESSIE PS ON FR.ParkeerSessie = PS.ReserveringsNr;
+FROM Factuur F
+LEFT JOIN FactuurRegel FR ON F.FactuurNr = FR.FactuurNr
+LEFT JOIN Adres A ON F.AdresId = A.AdresId
+LEFT JOIN Klant K ON F.KlantId = K.KlantId
+LEFT JOIN ParkeerPlaats PP ON FR.ParkeerPlaats = PP.ParkeerPlaatsId
+LEFT JOIN ParkeerSessie PS ON FR.ParkeerSessie = PS.ReserveringsNr;
 
 /* 4. Som de parkeeromgevingen op van veel naar weinig gebruikt */
 SELECT 
-    ParkeerOmgevingId AS Id,
-	CONCAT_WS(' ', Straat, HuisNr, Stad) AS ParkeerOmgeving,
-    COUNT(PS.ParkeerOmgeving) AS AantalKeer
-FROM PARKEEROMGEVING PO
-LEFT JOIN PARKEERSESSIE PS ON PO.ParkeerOmgevingId = PS.ParkeerOmgeving
-GROUP BY ParkeerOmgeving
+    ParkeerPlaatsId AS Id,
+	CONCAT_WS(' ', Straat, HuisNr, Stad) AS ParkeerPlaats,
+    COUNT(PS.ParkeerPlaats) AS AantalKeer
+FROM ParkeerPlaats PP
+LEFT JOIN ParkeerSessie PS ON PP.ParkeerPlaatsId = PS.ParkeerPlaats
+GROUP BY ParkeerPlaats
 ORDER BY AantalKeer DESC;
 
 /* 5. Haal de klant op die het meest heeft geparkeerd */
@@ -115,9 +116,9 @@ SELECT
     PS.Kenteken,
     CONCAT_WS(' ', Voornaam, Tussenvoegsel, Achternaam) AS KlantNaam,
     COUNT(PS.Kenteken) AS AantalKeer
-FROM PARKEERSESSIE PS
-LEFT JOIN VOERTUIG V ON PS.Kenteken = V.Kenteken
-LEFT JOIN KLANT K ON V.KlantId = K.KlantId
+FROM ParkeerSessie PS
+LEFT JOIN Voertuig V ON PS.Kenteken = V.Kenteken
+LEFT JOIN Klant K ON V.KlantId = K.KlantId
 GROUP BY PS.Kenteken
 ORDER BY AantalKeer DESC
 LIMIT 1;
