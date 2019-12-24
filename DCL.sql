301a330ed7c0c182d693fecc9ffc6fe320565571
Inge <ikerk79@hotmail.com>
2019-12-24 19:48:35 +0100

    New file for DCL users and roles

---
+++ DCL.sql
@@ -0,0 +1,13 @@
+/*
+********************************************************
+Name: PinPointParking
+Content: Data Control Language (DCL) for users and roles
+
+Users and roles: 
+- db_admin (ALL PRIVILIGES)
+- medewerker (INSERT, DELETE)
+- bedrijf (SELECT, INSERT, UPDATE, DELETE)
+- particulier (SELECT, INSERT, UPDATE, DELETE)
+********************************************************
+*/
+
