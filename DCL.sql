138edd5b94cb31dfed2d5afd4ee68fa47e4e2830
Inge <ikerk79@hotmail.com>
2019-12-30 11:35:22 +0100

    Added roles for db_admin and medewerker

--- DCL.sql
+++ DCL.sql
@@ -4,9 +4,9 @@ Name: PinPointParking
 Content: Data Control Language (DCL) for users and roles
 
 Users and roles: 
-1 db_admin (ALL PRIVILIGES)
+1 db_admin (ALL PRIVILIGES) met role ppp_dev
   Geen restrictions
-2 Medewerker (SELECT, DELETE)
+2 Medewerker (SELECT, DELETE) met role ppp_medewerker
   Userrestriction tables
 3 Bedrijf (SELECT, INSERT, UPDATE, DELETE)
   Views voor userrestriction
@@ -23,24 +23,40 @@ Users and roles:
     CREATE USER IF NOT EXISTS db_admin@localhost
     IDENTIFIED BY '11-11#11';
 
-    /* Check settings priviliges */
-    /* SHOW GRANTS FOR db_admin@localhost; */
+    CREATE USER IF NOT EXISTS db_admin2@localhost
+    IDENTIFIED BY '22-22#22';
 
-    GRANT ALL
+    /* Check settings priviliges */
+    /* SHOW GRANTS FOR db_admin@localhost;
+    SHOW GRANTS FOR db_admin2@localhost;  */
+
+    /* Create Role voor alle db_admin accounts */
+    CREATE ROLE ppp_dev;
+
+    /* Set Grants for role ppp_dev */
+    GRANT ALL 
     ON pinpointparking.* 
-    TO db_admin@localhost;
+    TO ppp_dev;
+
+    /* Check new priviliges voor role ppp_dev
+    SHOW GRANTS FOR ppp_dev; */
+    
+    /* Grant role to db_admin users */
+    GRANT ppp_dev 
+    TO db_admin@localhost, 
+       db_admin2@localhost;
 
     /* Check new priviliges */
-    /* SHOW GRANTS FOR db_admin@localhost; */
+    /* SHOW GRANTS FOR db_admin@localhost;
+    SHOW GRANTS FOR db_admin2@localhost; */
+    
+    /* To revoke grants */
+    /* REVOKE ALL PRIVILEGES, 
+    GRANT OPTION 
+    FROM db_admin@localhost; */
 
-
-    CREATE ROLE
-        ppp_dev@localhost,
-        ppp_mw@localhost;
-
-
-
-
+    /* To drop roles */
+    /* DROP ROLE ppp_dev */
 
 /**
 * 2. Create user medewerker and priviliges 
@@ -48,37 +64,66 @@ Users and roles:
     CREATE USER IF NOT EXISTS medewerker@localhost
     IDENTIFIED BY '1-11#1';
 
-    /* Check settings priviliges */
-    /* SHOW GRANTS FOR medewerker@localhost; */
+    CREATE USER IF NOT EXISTS medewerker2@localhost
+    IDENTIFIED BY '2-22#2';
 
+    CREATE USER IF NOT EXISTS medewerker3@localhost
+    IDENTIFIED BY '3-33#3';
+
+    /* Check settings priviliges */
+    /* SHOW GRANTS FOR medewerker@localhost;
+    SHOW GRANTS FOR medewerker2@localhost;
+    SHOW GRANTS FOR medewerker3@localhost; */
+
+    /* Create role voor alle medewerker accounts */
+    CREATE ROLE ppp_medewerker;
+
+    /* Set Grants for role ppp_medewerker
+     */
     GRANT SELECT
     ON pinpointparking.* 
-    TO medewerker@localhost;
+    TO ppp_medewerker;
 
     GRANT DELETE
     ON pinpointparking.Klant
-    TO medewerker@localhost;
+    TO ppp_medewerker;
 
     GRANT DELETE
     ON pinpointparking.Zakelijk
-    TO medewerker@localhost;
+    TO ppp_medewerker;
 
     GRANT DELETE
     ON pinpointparking.Adres
-    TO medewerker@localhost;
+    TO ppp_medewerker;
         
     GRANT DELETE
     ON pinpointparking.Voertuig
-    TO medewerker@localhost;
+    TO ppp_medewerker;
 
     /* Medewerker kan berekeningen met de stored procedure uitvoeren */
     GRANT EXECUTE 
     ON PROCEDURE pinpointparking.kostenParkeerPlaatsId1 
-    TO medewerker@localhost;
+    TO ppp_medewerker;
     
-/* Check new priviliges */
-/* SHOW GRANTS FOR medewerker@localhost; */
+    /* Grant role to medewerker users */
+    GRANT ppp_medewerker 
+    TO medewerker@localhost, 
+        medewerker2@localhost,
+        medewerker3@localhost;
 
+    /* Check new priviliges */
+    /* SHOW GRANTS FOR medewerker@localhost;
+    SHOW GRANTS FOR medewerker2@localhost;
+    SHOW GRANTS FOR medewerker3@localhost; */
+
+    /* To revoke grants */
+    /* REVOKE ALL PRIVILEGES, 
+    GRANT OPTION 
+    FROM medewerker@localhost; */
+
+    /* To drop role */
+    /* DROP ROLE ppp_medewerker */
+    
 /**
 * 3. Create user bedrijf Henneken and priviliges 
 */
@@ -120,12 +165,13 @@ Users and roles:
     LEFT JOIN Klant K ON V.KlantId = K.KlantId
     WHERE KvkNummer = 12345678;
 
+    /* Check results from Views */
     /* SELECT * FROM klantHenneken;
     SELECT * FROM zakelijkHenneken; 
     SELECT * FROM adresHenneken;
     SELECT * FROM voertuigHenneken; */
 
-    /* Only grant rows from view klantHenneken, zakelijkHenneken en adresHenneken */
+    /* Only grant permissions from Views klantHenneken, zakelijkHenneken en adresHenneken */
     GRANT SELECT, INSERT, UPDATE, DELETE
     ON pinpointparking.klantHenneken
     TO bedrijfHenneken@localhost;
@@ -154,7 +200,7 @@ Users and roles:
     /* Check settings priviliges */
     /* SHOW GRANTS FOR newparticulier@localhost; */
 
-    /* Only grant insertion */
+    /* Only grant permission to insert */
     GRANT INSERT
     ON pinpointparking.klant
     TO newparticulier@localhost;
@@ -203,6 +249,7 @@ Users and roles:
     LEFT JOIN Klant K ON V.KlantId = K.KlantId
     WHERE V.KlantId = 1;
 
+    /* Check results from Views */
     /* SELECT * FROM klantId1;
     SELECT * FROM adresKlantId1;
     SELECT * FROM voertuigKlantId1; */
